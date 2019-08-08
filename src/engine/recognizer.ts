99d8a2895e2f60823d35a30abd5aa7b62b72836f
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-08-09 01:34:09 +0300

    1.0.3

--- src/engine/recognizer.ts
+++ src/engine/recognizer.ts
@@ -53,7 +53,7 @@ export class Recognizer {
 
 	public remove(
 		socket: SocketIO.Socket
-	) {
+	): void {
 		let user = this.store.getUserBySocketId(
 			socket.id
 		);
@@ -75,15 +75,11 @@ export class Recognizer {
 		return this.store.getAllUsers();
 	}
 
-	public users(): User[] {
-		return this.store.getAllUsers();
-	}
-
 	public send(
 		event: string,
 		data: any,
 		recipientId: string
-	) {
+	): void {
 		let recipient = this.store.getUserById(
 			recipientId
 		);
@@ -101,7 +97,7 @@ export class Recognizer {
 	public sendToEveryone(
 		event: string,
 		data: any
-	) {
+	): void {
 		this.configuration.socketIO.emit(
 			event,
 			data
