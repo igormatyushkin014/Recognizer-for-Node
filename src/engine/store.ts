d240e36152a074cc89efe7abc1de9f18976cfd39
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-08-09 00:25:17 +0300

    Implemented Store

---
+++ src/engine/store.ts
@@ -0,0 +1,74 @@
+import {
+	IdProvider
+} from "./id-provider";
+
+import {
+	User
+} from "../data/user";
+
+export class Store {
+
+	private users = new Array<User>();
+
+	private idProvider = new IdProvider();
+
+	constructor() {
+	}
+
+	public getAllUsers(): User[] {
+		return this.users.slice();
+	}
+
+	public getUserById(id: string): User | undefined {
+		return this.users
+			.find((user) => {
+				return user.id === id;
+			});
+	}
+
+	public getUserBySocketId(id: string): User | undefined {
+		return this.users
+			.find((user) => {
+				return user.socket.id === id;
+			});
+	}
+
+	public createUser(
+		socket: SocketIO.Socket
+	): User {
+		let user: User = {
+			id: this.idProvider.getNextId(),
+			socket: socket
+		};
+		this.users.push(
+			user
+		);
+		return user;
+	}
+
+	public removeUserById(
+		id: string
+	) {
+		let index = this.users
+			.findIndex((user) => {
+				return user.id === id;
+			});
+
+		if (index >= 0 && index < this.users.length) {
+			this.users.splice(index, 1);
+		}
+	}
+
+	public removeUserBySocketId(
+		id: string
+	) {
+		let index = this.users
+			.findIndex((user) => {
+				return user.socket.id === id;
+			});
+
+		if (index >= 0 && index < this.users.length) {
+			this.users.splice(index, 1);
+		}
+	}
+}
